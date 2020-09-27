9127dabdb556895ad85df94e4fdf3bdec918292b
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2020-09-27 12:09:52 +0200

    add app routing with register and shared modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) }, 
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/modules/register/register.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+import { RegisterRoutingModule } from './register-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    SharedModule,
+    RegisterRoutingModule
+  ]
+})
+export class RegisterModule { }
