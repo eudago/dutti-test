028789e07c00fbdb6c512bac6b2bf0146fbd55ad
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2020-09-28 21:46:20 +0200

    cache getStarships

--- src/app/modules/ships/ships.service.ts
+++ src/app/modules/ships/ships.service.ts
@@ -1,15 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { Ship, ShipPagination } from 'src/app/interfaces/ship';
+
+const defaultUrl = 'https://swapi.dev/api/starships';
+const cacheTime = 300000; // 5 minutes
 
 @Injectable()
 export class ShipsService {
 
+  ships$: Observable<ShipPagination>;
+
   constructor(private http: HttpClient) { }
 
-  getStarships(url = 'https://swapi.dev/api/starships'): Observable<ShipPagination> {
-    return this.http.get<ShipPagination>(url);
+  getStarships(url = defaultUrl): Observable<ShipPagination> {
+    if((url === defaultUrl && !this.ships$) || url != defaultUrl ) {
+      this.ships$ = this.http.get<ShipPagination>(url).pipe(
+        shareReplay({ bufferSize: 1, refCount: true, windowTime: cacheTime })
+      );  
+    }
+
+    return this.ships$;
   }
 
   findById(id): Observable<Ship> {
